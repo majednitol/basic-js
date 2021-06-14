bac994d170a9db7cdf5511245f07735f123da9d6
majednitol <78228005+majednitol@users.noreply.github.com>
2021-06-14 21:11:39 +0600

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,60 @@
+//Arithmetic operator 
+//+ - / % ++ --
+var a = 11
+var b  = 6 
+console.log(a%b);
+
+//incremental
+// preincremental(++a ) , postincremental(a++)
+console.log(a++); //a= a+1 
+console.log(a);
+console.log(++a); //a+1 = a
+//decremental
+// predecremental(--a ) , postdecremental(a--)
+console.log(a--); //a= a-1 
+console.log(a);
+console.log(--a); //a-1 =a
+  
+
+//assignment operator
+
+
+var a = 10 
+var b = 20 
+ a+=b
+ console.log(a);
+ a-=b
+ console.log(a);
+ a*=b
+ console.log(a); 
+ a/=b
+ console.log(a);
+ a%=b
+ console.log(a); 
+
+
+ //comparison operator 
+
+ var a = 10 
+ var b =20 
+ console.log(a==b);
+ console.log(a!==b);
+ console.log(a>b);
+ console.log(a<b);
+ a = 20
+ console.log(a>=b);
+ console.log(a<=b);
+ var c = 50
+ var d= 50
+ console.log(c===d);
+ console.log(c!==d);
+
+ //logical operator
+ // && || !
+  
+ //bitwise operator [problem]
+ // & | ~ ^ << >>
+  
+ // how to check type 
+ console.log(typeof(10));
+
